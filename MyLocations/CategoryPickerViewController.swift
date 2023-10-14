b2f300cabb81023b39c584712115bd83449ef915
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2023-10-14 21:33:24 +0200

    selectedCategoryName property

---
+++ MyLocations/CategoryPickerViewController.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryPickerViewController.swift
+//  MyLocations
+//
+//  Created by Maryna Bolotska on 14/10/23.
+//
+
+import Foundation
---
+++ MyLocations/LocationDetailsViewController.swift
@@ -0,0 +1,86 @@
+//
+//  LocationDetailsViewController.swift
+//  MyLocations
+//
+//  Created by Maryna Bolotska on 09/10/23.
+//
+
+import UIKit
+import CoreLocation
+
+class LocationDetailsViewController: UITableViewController {
+
+    @IBOutlet var descriptionTextView: UITextView!
+    @IBOutlet var categoryLabel: UILabel!
+    @IBOutlet var latitudeLabel: UILabel!
+    @IBOutlet var longitudeLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+    private let dateFormatter: DateFormatter = {
+      let formatter = DateFormatter()
+      formatter.dateStyle = .medium
+      formatter.timeStyle = .short
+      return formatter
+    }()
+    
+    
+    // MARK: - Actions
+    @IBAction func done() {
+      navigationController?.popViewController(animated: true)
+    }
+    @IBAction func cancel() {
+      navigationController?.popViewController(animated: true)
+    }
+
+    
+    var coordinate = CLLocationCoordinate2D(
+      latitude: 0,
+      longitude: 0)
+    var placemark: CLPlacemark?
+    
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      descriptionTextView.text = ""
+      categoryLabel.text = ""
+      latitudeLabel.text = String(
+        format: "%.8f",
+        coordinate.latitude)
+      longitudeLabel.text = String(
+        format: "%.8f",
+        coordinate.longitude)
+      if let placemark = placemark {
+        addressLabel.text = string(from: placemark)
+    } else {
+        addressLabel.text = "No Address Found"
+      }
+      dateLabel.text = format(date: Date())
+    }
+    
+    // MARK: - Helper Methods
+    func string(from placemark: CLPlacemark) -> String {
+      var text = ""
+      if let tmp = placemark.subThoroughfare {
+        text += tmp + " "
+      }
+      if let tmp = placemark.thoroughfare {
+        text += tmp + ", "
+      }
+      if let tmp = placemark.locality {
+        text += tmp + ", "
+      }
+      if let tmp = placemark.administrativeArea {
+        text += tmp + " "
+      }
+      if let tmp = placemark.postalCode {
+        text += tmp + ", "
+      }
+       if let tmp = placemark.country {
+     text += tmp }
+     return text }
+    
+    
+    func format(date: Date) -> String {
+      return dateFormatter.string(from: date)
+    }
+}
