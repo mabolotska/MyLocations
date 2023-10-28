ae2d0cd99cab045b68a54d98a3c49156019dbf1d
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2023-10-28 11:15:07 +0200

    Fix the edit screen

---
+++ MyLocations/LocationCell.swift
@@ -0,0 +1,23 @@
+//
+//  LocationCell.swift
+//  MyLocations
+//
+//  Created by Maryna Bolotska on 28/10/23.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
